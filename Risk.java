a2b6ec6b6747d9f22a87446e9611e77f8ac0df93
BrightBlueBird <99009695+BrightBlueBird@users.noreply.github.com>
2022-03-15 14:33:06 +0100

    First commit

---
+++ Risk.java
@@ -0,0 +1,25 @@
+public class Risk {
+  private int pro;
+  private int con;
+  private int sev;
+
+  void setPro (int pro) {
+    this.pro = pro;
+  }
+  void setCon (int con) {
+    this.con = con;
+  }
+  void setSev (int sev) {
+    this.sev = sev;
+  }
+
+  int getPro() {
+    return pro;
+  }
+  int getCon() {
+    return con;
+  }
+  int getSev() {
+    return sev;
+  }
+}
